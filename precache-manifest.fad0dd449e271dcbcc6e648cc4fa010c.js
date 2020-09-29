0d9ca570169836473c5580d7dabb03af7733b00e
Jacek Paciorek <jack.paciorek@gmail.com>
2020-09-29 18:45:14 +0200

    Updates

---
+++ precache-manifest.fad0dd449e271dcbcc6e648cc4fa010c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e836cc923456ab594d682dc695c01d33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6608dee5d9164d289d5e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "64aceb264aa252659005",
+    "url": "/static/js/2.336e5329.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.336e5329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6608dee5d9164d289d5e",
+    "url": "/static/js/main.bbe51c44.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
