6381914f2ae863679c09acd636d9fabbf5c0d915
Jacek Paciorek <jack.paciorek@gmail.com>
2020-09-29 20:34:06 +0200

    Updates

--- precache-manifest.994586f029482c92c8443b6ca8240378.js
+++ precache-manifest.994586f029482c92c8443b6ca8240378.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e836cc923456ab594d682dc695c01d33",
+    "revision": "260546c9a0c05d827114df3da956dddb",
     "url": "/index.html"
   },
   {
-    "revision": "6608dee5d9164d289d5e",
+    "revision": "042a8e21c224752ef145",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "64aceb264aa252659005",
-    "url": "/static/js/2.336e5329.chunk.js"
+    "revision": "79268aa5f586ea1f730c",
+    "url": "/static/js/2.5c099392.chunk.js"
   },
   {
     "revision": "c87550333da8df8ab4c80bd1d6e9defb",
-    "url": "/static/js/2.336e5329.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.5c099392.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6608dee5d9164d289d5e",
-    "url": "/static/js/main.bbe51c44.chunk.js"
+    "revision": "042a8e21c224752ef145",
+    "url": "/static/js/main.81dc6406.chunk.js"
   },
   {
     "revision": "4f2b1704eab49d50853b",
